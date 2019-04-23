6f71d67605253fbc6d90e1cdfcf1f5ec466a5b26
Lungile Madi <lungie@mail.com>
2019-04-23 03:23:21 +0200

    'filesAndRanksArrays'

--- js/defs.js
+++ js/defs.js
@@ -69,3 +69,9 @@ var SQUARES = {
 
 var BOOL = {FALSE: 0, TRUE: 1}
 
+var FilesBrd = new Array(BRD_SQ_NUM)
+var RanksBrd = new Array(BRD_SQ_NUM)
+
+function FR2SQ(f,r) {
+    return ((21 + f) + (r * 10))
+}
