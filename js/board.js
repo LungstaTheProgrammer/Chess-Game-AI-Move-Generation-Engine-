bf280928b40cefe395ae0b4e28ed7bcaa4ece1e9
Lungile Madi <lungie@mail.com>
2019-04-23 21:31:56 +0200

    'squareAttack'

--- js/board.js
+++ js/board.js
@@ -248,5 +248,94 @@ function ParseFen(fen) {
 
     GameBoard.posKey = GeneratePosKey()
     UpdateListsMaterial()
+    PrintSqAttacked()
     //check if fen string is correct or throw exception
+}
+
+function PrintSqAttacked() {
+    var sq, file, rank, piece
+
+    console.log("\n Attacked: \n")
+
+    for (rank = RANKS.RANK_8; rank >= RANKS.RANK_1; rank--) {
+        var line = ((rank + 1) + "  ")
+        for (file = FILES.FILE_A; file <= FILES.FILE_H; file++) {
+            sq = FR2SQ(file, rank)
+            if (SqAttacked(sq, GameBoard.side) == BOOL.TRUE) piece = "X";
+            else piece = "-";
+            line += (" " + piece + " ")
+        }
+        console.log(line)
+    }
+    console.log("")
+}
+
+function SqAttacked(sq, side) {
+    var pce
+    var t_sq
+    var index
+
+    //Pawn
+    if (side == COLOURS.WHITE) {
+        // if white is top-bottom sq - 11 and sq - 9
+        // if white is bottom-top sq + 11 and sq + 9
+        // can just keep board this way in the back and 
+        // flip it on one player's side in the front
+        if (GameBoard.pieces[sq - 11] == PIECES.wP || GameBoard.pieces[sq - 9] == PIECES.wP) {
+            return BOOL.TRUE
+        }
+    } else {
+        if (GameBoard.pieces[sq + 11] == PIECES.bP || GameBoard.pieces[sq + 9] == PIECES.bP) {
+            return BOOL.TRUE
+        }
+    }
+    //Knight
+    for (index = 0; index < 8; index++) {
+        pce = GameBoard.pieces[sq + KnDir[index]]
+        if (pce != SQUARES.OFFBOARD && PieceCol[pce] == side && PieceKnight[pce] == BOOL.TRUE) {
+            return BOOL.TRUE
+        }
+    }
+    //Rook Queen
+    for (index = 0; index < 4; index++) {
+        dir = RkDir[index]
+        t_sq = sq + dir
+        pce = GameBoard.pieces[t_sq]
+        while(pce != SQUARES.OFFBOARD) {
+            if (pce != PIECES.EMPTY) {
+                if (PieceRookQueen[pce] == BOOL.TRUE && PieceCol[pce] == side) {
+                    return BOOL.TRUE
+                }
+                break
+            }
+            t_sq += dir
+            pce = GameBoard.pieces[t_sq]
+        }
+    }
+    //Bishop Queen
+    for (index = 0; index < 4; index++) {
+        var dir = BiDir[index]
+        t_sq = sq + dir
+        pce = GameBoard.pieces[t_sq]
+        while(pce != SQUARES.OFFBOARD) {
+            if (pce != PIECES.EMPTY) {
+                if (PieceBishopQueen[pce] == BOOL.TRUE && PieceCol[pce] == side) {
+                    return BOOL.TRUE
+                }
+                break
+            }
+            t_sq += dir
+            pce = GameBoard.pieces[t_sq]
+        }
+    }
+
+    //King
+    for (index = 0; index < 8; index++) {
+        pce = GameBoard.pieces[sq + KiDir[index]]
+        if (pce != SQUARES.OFFBOARD && PieceCol[pce] == side && PieceKing[pce] == BOOL.TRUE) {
+            return BOOL.TRUE
+        }
+    }
+
+    return BOOL.FALSE
 }
